1778e72b867a3d53e55166a329ec04b68cd9aa4e
awgn <awgn@antifork.org>
2007-05-26 13:22:47 +0000

    added fast_deque

--- codes++/fast_deque.hh
+++ codes++/fast_deque.hh
@@ -1,3 +1,12 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli
+ * ----------------------------------------------------------------------------
+ */
+
 #ifndef FAST_DEQUE_HH
 #define FAST_DEQUE_HH
 
