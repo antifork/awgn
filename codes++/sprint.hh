100a9ebb68eb317f752d360226ede6061609726d
awgn <awgn@antifork.org>
2007-09-04 20:48:06 +0000

    minor changes in sprint

--- codes++/sprint.hh
+++ codes++/sprint.hh
@@ -9,13 +9,24 @@
 #ifndef SPRINT_HH
 #define SPRINT_HH
 
+#include <cstdio>
 #include <iostream>
 #include <string>
 
 namespace extra {
 
+    static std::string sprint(const char *fmt) __attribute__((unused));
+    static std::string sprint(const char *fmt) 
+        {
+            char *strp;
+            if ( asprintf(&strp, fmt) == -1 )
+                return std::string();
+            std::string ret(strp); 
+            free(strp);
+            return ret;
+        }
     template <typename T1>
-        std::string sprint(const char *fmt, T1 arg_1) 
+    static  std::string sprint(const char *fmt, T1 arg_1) 
         {
             char *strp;
             if ( asprintf(&strp, fmt, arg_1) == -1 )
@@ -25,7 +36,7 @@ namespace extra {
             return ret;
         }
     template <typename T1, typename T2>
-        std::string sprint(const char *fmt, T1 arg_1, T2 arg_2) 
+    static std::string sprint(const char *fmt, T1 arg_1, T2 arg_2) 
         {
             char *strp;
             if ( asprintf(&strp, fmt, arg_1, arg_2) == -1 )
@@ -35,7 +46,7 @@ namespace extra {
             return ret;
         }
     template <typename T1, typename T2, typename T3>
-        std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3) 
+    static std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3) 
         {
             char *strp;
             if ( asprintf(&strp, fmt, arg_1, arg_2, arg_3) == -1 )
@@ -45,7 +56,7 @@ namespace extra {
             return ret;
         }
     template <typename T1, typename T2, typename T3, typename T4>
-        std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3, T4 arg_4) 
+    static std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3, T4 arg_4) 
         {
             char *strp;
             if ( asprintf(&strp, fmt, arg_1, arg_2, arg_3, arg_4) == -1 )
@@ -55,7 +66,7 @@ namespace extra {
             return ret;
         }
     template <typename T1, typename T2, typename T3, typename T4, typename T5>
-        std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3, T4 arg_4, T5 arg_5) 
+    static std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3, T4 arg_4, T5 arg_5) 
         {
             char *strp;
             if ( asprintf(&strp, fmt, arg_1, arg_2, arg_3, arg_4, arg_5) == -1 )
@@ -65,7 +76,7 @@ namespace extra {
             return ret;
         }
     template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
-        std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3, T4 arg_4, T5 arg_5, T6 arg_6) 
+    static std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3, T4 arg_4, T5 arg_5, T6 arg_6) 
         {
             char *strp;
             if ( asprintf(&strp, fmt, arg_1, arg_2, arg_3, arg_4, arg_5, arg_6) == -1 )
@@ -75,7 +86,7 @@ namespace extra {
             return ret;
         }
     template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7>
-        std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3, T4 arg_4, T5 arg_5, T6 arg_6, T7 arg_7) 
+    static std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3, T4 arg_4, T5 arg_5, T6 arg_6, T7 arg_7) 
         {
             char *strp;
             if ( asprintf(&strp, fmt, arg_1, arg_2, arg_3, arg_4, arg_5, arg_6, arg_7) == -1 )
@@ -85,7 +96,7 @@ namespace extra {
             return ret;
         }
     template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8>
-        std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3, T4 arg_4, T5 arg_5, T6 arg_6, T7 arg_7, T8 arg_8) 
+    static std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3, T4 arg_4, T5 arg_5, T6 arg_6, T7 arg_7, T8 arg_8) 
         {
             char *strp;
             if ( asprintf(&strp, fmt, arg_1, arg_2, arg_3, arg_4, arg_5, arg_6, arg_7, arg_8) == -1 )
@@ -95,7 +106,7 @@ namespace extra {
             return ret;
         }
     template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9>
-        std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3, T4 arg_4, T5 arg_5, T6 arg_6, T7 arg_7, T8 arg_8, T9 arg_9) 
+    static std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3, T4 arg_4, T5 arg_5, T6 arg_6, T7 arg_7, T8 arg_8, T9 arg_9) 
         {
             char *strp;
             if ( asprintf(&strp, fmt, arg_1, arg_2, arg_3, arg_4, arg_5, arg_6, arg_7, arg_8, arg_9) == -1 )
@@ -105,7 +116,7 @@ namespace extra {
             return ret;
         }
     template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10>
-        std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3, T4 arg_4, T5 arg_5, T6 arg_6, T7 arg_7, T8 arg_8, T9 arg_9, T10 arg_10) 
+    static std::string sprint(const char *fmt, T1 arg_1, T2 arg_2, T3 arg_3, T4 arg_4, T5 arg_5, T6 arg_6, T7 arg_7, T8 arg_8, T9 arg_9, T10 arg_10) 
         {
             char *strp;
             if ( asprintf(&strp, fmt, arg_1, arg_2, arg_3, arg_4, arg_5, arg_6, arg_7, arg_8, arg_9, arg_10) == -1 )
@@ -114,6 +125,7 @@ namespace extra {
             free(strp);
             return ret;
         }
+
 }
 
 #endif /* SPRINT_HH */
