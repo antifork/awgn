27a41cddc4bc23a3cb1e989978446f95a6a7b77e
awgn <awgn@antifork.org>
2008-03-22 20:08:52 +0000

    singleton process based on flock

--- codes++/singleton_proc.hh
+++ codes++/singleton_proc.hh
@@ -11,11 +11,8 @@
 #ifndef SINGLETON_PROC_HH
 #define SINGLETON_PROC_HH
 
-#include <sys/types.h>          /* See NOTES */
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
+#include <sys/file.h>
+#include <errno.h>
 
 #include <iostream>
 #include <stdexcept>
@@ -24,58 +21,31 @@ extern char *__progname;
 
 namespace more 
 {
-
     class singleton_proc 
     {
-        char lockf[24];
-        int fd, s;
-
+        int _M_fd;
     public:
-        singleton_proc(unsigned short port) 
-        : lockf(),
-          fd(),
-          s()
+        singleton_proc(const char *filelock) 
+        : _M_fd(open(filelock, O_RDONLY|O_CREAT))
         {
-            ::sprintf(lockf,"/tmp/singleton:%d",port);
-            fd = open(lockf, O_CREAT|O_EXCL);
-            if (fd == -1) 
-                throw std::runtime_error
-                (std::string("a session of '").append(__progname).append("' is already running."));
+            if (_M_fd == -1) 
+                throw std::runtime_error (std::string("couldn't open lockfile"));
 
-            try {
-
-                if ((s = ::socket(AF_INET, SOCK_STREAM, 0)) == -1)
-                    throw std::runtime_error("socket");
-
-                int on = 1;
-                if (::setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(int)) == -1)
-                    throw std::runtime_error("setsockopt");
-
-                struct sockaddr_in addr;
-                addr.sin_family      = AF_INET;
-                addr.sin_port        = htons(port);
-                addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-
-                if (::bind(s, reinterpret_cast<const struct sockaddr *>(&addr), sizeof(struct sockaddr_in)) == -1) 
+            if (flock(_M_fd, LOCK_EX|LOCK_NB) < 0 ) {
+                if (errno == EWOULDBLOCK)
                     throw std::runtime_error(std::string("a session of '").append(__progname).append("' is already running."));
-
-                if (::listen(s,1) == -1) 
-                    throw std::runtime_error("listen");
-
-                ::close(fd);
-                ::unlink(lockf);
-
-            }
-            catch(...) {
-                ::close(fd);
-                ::unlink(lockf);
-                throw;
-            }
+                else
+                    throw std::runtime_error(std::string("flock: " + std::string(strerror(errno))));
+           }
         }
 
         ~singleton_proc() {
-            ::close(fd);
-            ::unlink(lockf);
+            if (flock(_M_fd, LOCK_UN) < 0 ) {
+                std::clog << "flock(...,LOCK_UN: " << strerror(errno) << std::endl;
+                return;
+            }
+            close(_M_fd);
+
         }
     };
 
