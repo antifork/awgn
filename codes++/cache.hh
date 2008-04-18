1becd4d82cdcbb2a29a9b10e1f1c71c45cb02f7a
awgn <awgn@antifork.org>
2008-04-18 18:39:21 +0000

    bugs and style fixed

--- codes++/cache.hh
+++ codes++/cache.hh
@@ -23,32 +23,56 @@ namespace generic
 {
     template <class KEY, class VALUE>
     class cache {
-
     public:
 
         template <typename T>
         class proxy {
 
         public:
+            typedef T value_type;
+
             proxy(const T &r = T(), int t=0) 
             : _M_data(r), 
               _M_timeout(t),
               _M_timestamp()
-            { ts_update(); }
+            { this->ts_update(); }
+
+            proxy(const proxy &rhs)
+            {
+                _M_data = rhs._M_data;
+                _M_timeout = rhs._M_timeout;
+                this->ts_update();
+            }
 
             proxy & 
-            operator=(const T r) 
+            operator=(const proxy &rhs)
+            {
+                if ( &_M_data == &rhs ) 
+                    return *this;
+                this->_M_data = rhs._M_data;
+                this->_M_timeout = rhs._M_timeout;
+                this->ts_update();
+                return *this;
+            }
+
+            proxy & 
+            operator=(const T &rhs) 
             { 
-                _M_data = r; 
-                ts_update(); 
+                _M_data = rhs; 
+                this->ts_update(); 
                 return *this; 
             }
 
-            operator T&() 
-            { return _M_data; }
-
-            operator T*() 
+            T *
+            operator&()
             { return &_M_data; }
+
+            const T *
+            operator&() const
+            { return &_M_data; }
+
+            operator T()
+            { return _M_data; }
 
             void 
             ts_update(int timeo = -1) 
@@ -62,22 +86,16 @@ namespace generic
             bool 
             expired() const 
             {
-                if (_M_timeout == 0)
+                if (_M_timeout == 0) {
                     return false;   // never expires
+                }
                 struct timeval now;
                 gettimeofday(&now,NULL);
                 return static_cast<unsigned int>(now.tv_sec) > (_M_timestamp + _M_timeout);
             }
 
-            void 
-            dump(std::ostream& o) const 
-            { 
-                o << "data:" << _M_data << " timestamp:" << _M_timestamp << " timeout:" << _M_timeout << std::endl;
-            }
-
         private:
             T _M_data;
-
             unsigned int _M_timeout;
             unsigned int _M_timestamp;
         };
@@ -86,8 +104,8 @@ namespace generic
         typedef typename std::map<KEY,proxy<VALUE> >::iterator iterator;
         typedef typename std::map<KEY,proxy<VALUE> >::const_iterator const_iterator;
 
-        cache() :
-        db()
+        cache() 
+        : _M_db()
         {}
 
         ~cache(){}
@@ -95,49 +113,49 @@ namespace generic
         VALUE *
         insert(KEY k, VALUE v, int t = 0) 
         {
-            typename db_T::iterator it = db.find(k);
-            if ( it != db.end() && !it->second.expired() ) 
+            typename db_T::iterator it = _M_db.find(k);
+            if ( it != _M_db.end() && !it->second.expired() ) 
                 throw std::runtime_error("key already in use!");
-            db[k] = proxy<VALUE>(v, t);
-            return db[k];
+            _M_db[k] = proxy<VALUE>(v, t);
+            return & _M_db[k];
         }
 
         VALUE *
         update(KEY k, VALUE v, int t = 0) 
         {
-            db[k] = proxy<VALUE>(v, t);
-            db[k].ts_update();
-            return db[k];
+            _M_db[k] = proxy<VALUE>(v, t);
+            _M_db[k].ts_update();
+            return & _M_db[k];
         }
 
         VALUE *
         update(KEY k, int t = 0) 
         {
-            typename db_T::iterator it = db.find(k);
-            if ( it == db.end() )
+            typename db_T::iterator it = _M_db.find(k);
+            if ( it == _M_db.end() )
                 throw std::runtime_error("key not present!");
             it->second.ts_update(t);
-            return db[k]; 
+            return & _M_db[k]; 
         }
 
         VALUE *
         find(KEY k, bool ts_update = false) 
         {
-            typename db_T::iterator it = db.find(k);
-            if( it == db.end()) 
+            typename db_T::iterator it = _M_db.find(k);
+            if( it == _M_db.end()) 
                 throw std::runtime_error("key not present!");
             if (it->second.expired())
                 throw static_cast<VALUE>(it->second);
             if (ts_update)
                 it->second.ts_update();
-            return it->second;
+            return & it->second;
         }
 
         bool 
-        haskey(KEY k, bool ts_update = false) 
+        has_key(KEY k, bool ts_update = false) 
         {
-            typename db_T::iterator it = db.find(k);
-            if ( it == db.end())
+            typename db_T::iterator it = _M_db.find(k);
+            if ( it == _M_db.end())
                 return false;
             if (it->second.expired())
                 return false;
@@ -148,26 +166,29 @@ namespace generic
 
         int 
         erase(KEY k) 
-        { return db.erase(k); }
+        { return _M_db.erase(k); }
+
+        // iterators...
+        //
 
         typename db_T::iterator 
         begin() 
-        { return  db.begin(); }
+        { return  _M_db.begin(); }
 
         typename db_T::iterator 
         end() 
-        { return db.end(); }
+        { return _M_db.end(); }
 
         typename db_T::const_iterator 
         begin() const 
-        { return  db.begin(); }
+        { return  _M_db.begin(); }
 
         typename db_T::const_iterator 
         end() const 
-        { return db.end(); }
+        { return _M_db.end(); }
 
     private:
-        db_T db;
+        db_T _M_db;
     };
 
 } // namespace generic
