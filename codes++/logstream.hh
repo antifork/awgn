46a347da6091fef9b95b9ae1ddc49e9aff0f34ca
awgn <awgn@antifork.org>
2008-11-12 22:24:37 +0000

    added sync()

--- codes++/logstream.hh
+++ codes++/logstream.hh
@@ -46,11 +46,16 @@ namespace more {
         void logprio(int n)
         { _M_leveltmp = n; }
 
+        static int iword_index()
+        { static int index = std::ios_base::xalloc();
+            return index; }
+
+    protected:
+
         virtual std::streamsize
         xsputn (const char *s, std::streamsize n)
         {
-            int current = (_M_leveltmp != -1 ? _M_leveltmp : _M_levelmsg);
-            if ( current <= _M_level) {
+            if ( _M_current() <= _M_level) {
                 return _M_out->sputn(s,n);
             }
             return n; 
@@ -59,24 +64,26 @@ namespace more {
         virtual int_type
         overflow (int_type c)
         {
-            int current = (_M_leveltmp != -1 ? _M_leveltmp : _M_levelmsg);
-            if (c == '\n')
-                _M_leveltmp = -1;
-
-            if (current <= _M_level) {
-                _M_out->sputc(c);
-                return ( _M_out->pubsync() == -1 ? EOF : c );
+            if (_M_current() <= _M_level) {
+                if (c == '\n')
+                    _M_leveltmp = -1;
+                return _M_out->sputc(c);
             }
             return c;
         }
 
-        static int iword_index()
+        int sync()
         {
-            static int index = std::ios_base::xalloc();
-            return index;
+            if (_M_current() <= _M_level)
+                return  _M_out->pubsync();
+            return 0;
         }
 
-    private:
+    private:        
+        
+        int _M_current() const 
+        { return _M_leveltmp != -1 ? _M_leveltmp : _M_levelmsg; }
+
         int _M_level;
         int _M_levelmsg;
         int _M_leveltmp;
